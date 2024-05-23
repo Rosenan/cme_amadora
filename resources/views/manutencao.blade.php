8cb0f127f56c697d0a38d4a3d1817891e3bc408c
Rosenan <rosenanmelo@gmail.com>
2024-05-23 05:24:16 +0100

    initial commit

---
+++ resources/views/manutencao.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<title>Site Maintenance</title>
+<style>
+  body { text-align: center; padding: 150px; }
+  h1 { font-size: 50px; }
+  body { font: 20px Helvetica, sans-serif; color: #333; }
+  article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+  a { color: #dc8100; text-decoration: none; }
+  a:hover { color: #333; text-decoration: none; }
+</style>
+
+<article>
+    <h1>Voltaremos em breve!</h1>
+    <div>
+        <p>Pedimos desculpa pelo incómodo, mas estamos a fazer uma manutenção neste momento. Se precisar, pode sempre <a href="mailto:catedralbrandoa2022@gmail.com">contactar-nos</a>, caso contrário, voltaremos a estar online em breve!
+</p>
+                <p>&mdash; CME Amadora</p>
+    </div>
+</article>
